b4fc39228b7f7891906c63be8ab72cb8ecadaf04
Neil Pandya <dev@neilpandya.com>
2026-02-21 10:58:47 +0100

    Refactor tray to use glib idle_add_local

--- src/tray.rs
+++ src/tray.rs
@@ -1,92 +1,87 @@
 // src/tray.rs
 // Manages the system tray UI thread to provide visual status and color information to the user.
 
-use gtk;
-use std::sync::mpsc::Receiver;
+use gtk::glib;
+use std::sync::mpsc;
 use std::thread;
 use tray_icon::{
     TrayIconBuilder,
     menu::{Menu, MenuItem},
 };
 
-// Private helpers
+/// Simple wrapper to send updates to the GTK thread.
+#[derive(Clone)]
+pub struct TraySender {
+    tx: mpsc::Sender<[u8; 3]>,
+}
 
-/// Updates the tray icon and menu items
+impl TraySender {
+    pub fn send(&self, rgb: [u8; 3]) {
+        if self.tx.send(rgb).is_ok() {
+            // Use the THREAD-SAFE idle_add_once.
+            // This is safe to call from the control loop thread.
+            glib::idle_add_once(move || {
+                // This empty closure just "pokes" the main loop to wake up
+                // and run the idle handler attached in spawn_tray.
+            });
+        }
+    }
+}
+
 fn update_tray_visuals(
     tray: &mut tray_icon::TrayIcon,
     hex_item: &MenuItem,
     rgb_item: &MenuItem,
     rgb: [u8; 3],
 ) {
-    // Update tray icon
     let img = crate::color::create_color_icon(rgb);
     let buf = img.into_vec();
     if let Ok(rgba_icon) = tray_icon::Icon::from_rgba(buf, 16, 16) {
         let _ = tray.set_icon(Some(rgba_icon));
     }
-
-    // Update menu text
     hex_item.set_text(&crate::color::format_hex_string(rgb));
     rgb_item.set_text(&crate::color::format_rgb_string(rgb));
 }
 
-/// Spawns the system tray UI thread with the provided color receiver
-pub fn spawn_tray(color_rx: Receiver<[u8; 3]>) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        // Initialize GTK in the tray thread
+pub fn spawn_tray() -> (TraySender, thread::JoinHandle<()>) {
+    let (tx, rx) = mpsc::channel::<[u8; 3]>();
+    let tray_sender = TraySender { tx };
+
+    let handle = thread::spawn(move || {
         if let Err(e) = gtk::init() {
-            log::error!("Failed to initialize GTK for tray: {}", e);
+            log::error!("Failed to initialize GTK: {}", e);
             return;
         }
 
-        // Create menu items
         let hex_item = MenuItem::new("HEX: #------", false, None);
         let rgb_item = MenuItem::new("RGB: ---,---,---", false, None);
-
-        // Create menu
         let menu = Menu::new();
-        if let Err(e) = menu.append(&hex_item) {
-            log::warn!("Failed to append hex item to menu: {}", e);
-        }
-        if let Err(e) = menu.append(&rgb_item) {
-            log::warn!("Failed to append rgb item in menu: {}", e);
-        }
+        let _ = menu.append(&hex_item).unwrap();
+        let _ = menu.append(&rgb_item).unwrap();
 
-        // Build tray
-        let tray_result = TrayIconBuilder::new()
+        let mut tray = TrayIconBuilder::new()
             .with_tooltip("Aura XDG-Accent Sync")
             .with_menu(Box::new(menu))
-            .build();
+            .build()
+            .expect("Failed to build tray icon");
 
-        match tray_result {
-            Ok(mut tray) => {
-                log::info!("System tray icon created successfully");
-
-                loop {
-                    // Pump GTK events
-                    while gtk::events_pending() {
-                        gtk::main_iteration();
-                    }
-
-                    // Update visuals for the LATEST color received
-                    let mut latest_color = None;
-                    while let Ok(color) = color_rx.try_recv() {
-                        latest_color = Some(color);
-                    }
-
-                    if let Some(color) = latest_color {
-                        update_tray_visuals(&mut tray, &hex_item, &rgb_item, color);
-                    }
-
-                    std::thread::sleep(std::time::Duration::from_millis(100));
-                }
+        // Inside the GTK thread, we use idle_add_local to monitor the channel.
+        // This is safe because this thread owns the MainContext.
+        glib::idle_add_local(move || {
+            let mut latest = None;
+            while let Ok(color) = rx.try_recv() {
+                latest = Some(color);
             }
 
-            Err(e) => {
-                log::error!("Failed to create system tray icon: {}", e);
-                // Document expected behavior if GTK initialization fails
-                log::warn!("Continuing to run without system tray icon");
+            if let Some(color) = latest {
+                update_tray_visuals(&mut tray, &hex_item, &rgb_item, color);
             }
-        }
-    })
+
+            glib::ControlFlow::Continue
+        });
+
+        gtk::main();
+    });
+
+    (tray_sender, handle)
 }
