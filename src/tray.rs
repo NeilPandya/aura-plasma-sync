7f45e8a5915438252ec384494d31d7eeb89bd589
Neil Pandya <dev@neilpandya.com>
2026-02-23 00:14:27 +0100

    Optimize tray UI updates with event-driven model

--- src/tray.rs
+++ src/tray.rs
@@ -1,15 +1,21 @@
 // src/tray.rs
-// Manages the system tray UI thread to provide visual status and color information to the user.
+// Manages the system tray UI thread efficiently using event-driven updates.
 
 use gtk::glib;
+use std::cell::RefCell;
 use std::sync::mpsc;
 use std::thread;
 use tray_icon::{
-    TrayIconBuilder,
+    TrayIcon, TrayIconBuilder,
     menu::{Menu, MenuItem},
 };
 
-/// Simple wrapper to send updates to the GTK thread.
+// Thread-local storage to hold UI handles safely within the GTK thread.
+thread_local! {
+    static TRAY_STATE: RefCell<Option<(TrayIcon, MenuItem, MenuItem)>> = const { RefCell::new(None) };
+    static TRAY_RX: RefCell<Option<mpsc::Receiver<[u8; 3]>>> = const { RefCell::new(None) };
+}
+
 #[derive(Clone)]
 pub struct TraySender {
     tx: mpsc::Sender<[u8; 3]>,
@@ -18,29 +24,38 @@ pub struct TraySender {
 impl TraySender {
     pub fn send(&self, rgb: [u8; 3]) {
         if self.tx.send(rgb).is_ok() {
-            // Use the THREAD-SAFE idle_add_once.
-            // This is safe to call from the control loop thread.
+            // Schedule a ONE-OFF execution on the GTK thread.
+            // This wakes the thread up, runs the closure once, then lets it sleep.
             glib::idle_add_once(move || {
-                // This empty closure just "pokes" the main loop to wake up
-                // and run the idle handler attached in spawn_tray.
+                TRAY_RX.with(|rx_cell| {
+                    if let Some(rx) = rx_cell.borrow().as_ref() {
+                        let mut latest = None;
+                        while let Ok(color) = rx.try_recv() {
+                            latest = Some(color);
+                        }
+
+                        if let Some(color) = latest {
+                            update_ui(color);
+                        }
+                    }
+                });
             });
         }
     }
 }
 
-fn update_tray_visuals(
-    tray: &mut tray_icon::TrayIcon,
-    hex_item: &MenuItem,
-    rgb_item: &MenuItem,
-    rgb: [u8; 3],
-) {
-    let img = crate::color::create_color_icon(rgb);
-    let buf = img.into_vec();
-    if let Ok(rgba_icon) = tray_icon::Icon::from_rgba(buf, 16, 16) {
-        let _ = tray.set_icon(Some(rgba_icon));
-    }
-    hex_item.set_text(&crate::color::format_hex_string(rgb));
-    rgb_item.set_text(&crate::color::format_rgb_string(rgb));
+fn update_ui(rgb: [u8; 3]) {
+    TRAY_STATE.with(|state_cell| {
+        if let Some((tray, hex_item, rgb_item)) = state_cell.borrow_mut().as_mut() {
+            let img = crate::color::create_color_icon(rgb);
+            let buf = img.into_vec();
+            if let Ok(rgba_icon) = tray_icon::Icon::from_rgba(buf, 16, 16) {
+                let _ = tray.set_icon(Some(rgba_icon));
+            }
+            hex_item.set_text(&crate::color::format_hex_string(rgb));
+            rgb_item.set_text(&crate::color::format_rgb_string(rgb));
+        }
+    });
 }
 
 pub fn spawn_tray() -> (TraySender, thread::JoinHandle<()>) {
@@ -56,29 +71,18 @@ pub fn spawn_tray() -> (TraySender, thread::JoinHandle<()>) {
         let hex_item = MenuItem::new("HEX: #------", false, None);
         let rgb_item = MenuItem::new("RGB: ---,---,---", false, None);
         let menu = Menu::new();
-        let _ = menu.append(&hex_item).unwrap();
-        let _ = menu.append(&rgb_item).unwrap();
+        let _ = menu.append(&hex_item);
+        let _ = menu.append(&rgb_item);
 
-        let mut tray = TrayIconBuilder::new()
+        let tray = TrayIconBuilder::new()
             .with_tooltip("Aura XDG-Accent Sync")
             .with_menu(Box::new(menu))
             .build()
             .expect("Failed to build tray icon");
 
-        // Inside the GTK thread, we use idle_add_local to monitor the channel.
-        // This is safe because this thread owns the MainContext.
-        glib::idle_add_local(move || {
-            let mut latest = None;
-            while let Ok(color) = rx.try_recv() {
-                latest = Some(color);
-            }
-
-            if let Some(color) = latest {
-                update_tray_visuals(&mut tray, &hex_item, &rgb_item, color);
-            }
-
-            glib::ControlFlow::Continue
-        });
+        // Initialize thread-local state
+        TRAY_STATE.with(|s| *s.borrow_mut() = Some((tray, hex_item, rgb_item)));
+        TRAY_RX.with(|r| *r.borrow_mut() = Some(rx));
 
         gtk::main();
     });
